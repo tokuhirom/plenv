16ce14a9b4e89aad369ca46e9a2892bc2e332ec7
tokuhirom <tokuhirom@gmail.com>
2013-01-13 16:20:03 +0900

    cpanize

--- lib/App/plenv.pm
+++ lib/App/plenv.pm
@@ -7,35 +7,3 @@ our $VERSION = '0.01';
 
 
 1;
-__END__
-
-=encoding utf8
-
-=head1 NAME
-
-App::plenv - ...
-
-=head1 SYNOPSIS
-
-  use App::plenv;
-
-=head1 DESCRIPTION
-
-App::plenv is
-
-B<THIS IS A DEVELOPMENT RELEASE. API MAY CHANGE WITHOUT NOTICE>.
-
-=head1 AUTHOR
-
-Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
-
-=head1 SEE ALSO
-
-=head1 LICENSE
-
-Copyright (C) Tokuhiro Matsuno
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
