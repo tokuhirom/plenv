af44673e3759209dbbe64fc4a28e7477885ad493
tokuhirom <tokuhirom@gmail.com>
2013-01-17 21:24:17 +0900

    mv local/ share/plenv.

---
+++ share/plenv/lib/lib/perl5/App/perlbuild.pm
@@ -0,0 +1,41 @@
+package App::perlbuild;
+use strict;
+use warnings;
+use 5.008005;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+App::perlbuild - ...
+
+=head1 SYNOPSIS
+
+  use App::perlbuild;
+
+=head1 DESCRIPTION
+
+App::perlbuild is
+
+B<THIS IS A DEVELOPMENT RELEASE. API MAY CHANGE WITHOUT NOTICE>.
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
