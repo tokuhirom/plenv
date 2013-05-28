23bce02b432b03313c9b0338ec52f8fe57758a79
tokuhirom <tokuhirom@gmail.com>
2013-05-28 23:11:53 +0900

    more replacements.

--- author/copy-from-rbenv.sh
+++ author/copy-from-rbenv.sh
@@ -2,6 +2,6 @@
 rm libexec/*
 cp ~/.rbenv/libexec/* libexec/
 rename 's/rbenv/plenv/' libexec/*
-perl -i -pe 's/RUBY/PERL/g; s/rbenv/plenv/g;s/RBENV/PLENV/g;s/Ruby/Perl/g;s/ruby/perl/g' libexec/*
+perl -i -pe 's!github.com/sstephenson/rbenv!github.com/tokuhirom/plenv!g; s/RUBY/PERL/g; s/rbenv/plenv/g;s/RBENV/PLENV/g;s/Ruby/Perl/g;s/ruby/perl/g' libexec/*
 chmod +x libexec/*
 grep sstephenson libexec/* plugins/*/*
