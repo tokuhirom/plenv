54dd40e9082c84b0bcb8e08b9237d807205aa737
tokuhirom <tokuhirom@gmail.com>
2013-05-30 05:25:09 +0900

    s/bundle install/carton install/

--- author/copy-from-rbenv.sh
+++ author/copy-from-rbenv.sh
@@ -5,7 +5,7 @@ rm -f completions/*
 cp ~/.rbenv/libexec/* libexec/
 cp ~/.rbenv/completions/* completions/
 rename 's/rbenv/plenv/' libexec/* completions/*
-/usr/bin/perl -i -pe 's!github.com/sstephenson/rbenv!github.com/tokuhirom/plenv!g; s/RUBY/PERL/g; s/rbenv/plenv/g;s/RBENV/PLENV/g;s/Ruby/Perl/g;s/ruby/perl/g; s/1.9.3-p327/5.18.0/g' libexec/* completions/*
+/usr/bin/perl -i -pe 's!github.com/sstephenson/rbenv!github.com/tokuhirom/plenv!g; s/RUBY/PERL/g; s/rbenv/plenv/g;s/RBENV/PLENV/g;s/Ruby/Perl/g;s/ruby/perl/g; s/1.9.3-p327/5.18.0/g; s/bundle install/carton install/g' libexec/* completions/*
 chmod +x libexec/*
 grep sstephenson libexec/* plugins/*/* completions/*
 
