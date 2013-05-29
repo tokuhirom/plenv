7b490e776c36986dfa3bc717cf9d9541e0082fff
tokuhirom <tokuhirom@gmail.com>
2013-05-29 18:28:36 +0900

    `plenv init --no-rehash` by default.

--- author/copy-from-rbenv.sh
+++ author/copy-from-rbenv.sh
@@ -5,6 +5,9 @@ rm -f completions/*
 cp ~/.rbenv/libexec/* libexec/
 cp ~/.rbenv/completions/* completions/
 rename 's/rbenv/plenv/' libexec/* completions/*
-perl -i -pe 's!github.com/sstephenson/rbenv!github.com/tokuhirom/plenv!g; s/RUBY/PERL/g; s/rbenv/plenv/g;s/RBENV/PLENV/g;s/Ruby/Perl/g;s/ruby/perl/g' libexec/* completions/*
+/usr/bin/perl -i -pe 's!github.com/sstephenson/rbenv!github.com/tokuhirom/plenv!g; s/RUBY/PERL/g; s/rbenv/plenv/g;s/RBENV/PLENV/g;s/Ruby/Perl/g;s/ruby/perl/g' libexec/* completions/*
 chmod +x libexec/*
 grep sstephenson libexec/* plugins/*/* completions/*
+
+# enable --no-rehash by default
+perl -i -pe 's/no_rehash=""/no_rehash=1/' libexec/plenv-init
