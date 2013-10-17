8778ca9735db0e340e1ea3d3770e1aef23937a73
Olivier Mengué <dolmen@cpan.org>
2013-10-17 19:17:10 +0200

    rehash_cpanm: use single quotes around pre-resolved command

--- plenv.d/rehash/rehash_cpanm.bash
+++ plenv.d/rehash/rehash_cpanm.bash
@@ -32,7 +32,7 @@ if [[ ! "\$CURRENT_PERL_VERSION" =~ $regexp_system_perl ]] &&
 fi
 
 export PLENV_ROOT="$PLENV_ROOT"
-"$(command -v plenv)" exec "\$program" "\$@"
+'$(command -v plenv)' exec "\$program" "\$@"
 rc=\$?
 for arg in \$@
 do
@@ -42,7 +42,7 @@ do
     ;;
   esac
 done
-"$(command -v plenv)" rehash
+'$(command -v plenv)' rehash
 exit \$rc
 CPANM_SHIM
     else
